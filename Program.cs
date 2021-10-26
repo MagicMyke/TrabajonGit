edfd3a914aec1a72d466e4e6171ee640d6872978
Gustavogba <gustavopgb21@gmail.com>
2021-10-26 09:56:17 -0400

    ejercicio 2

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace TrabajoGit
             int a;
 
             Console.WriteLine("Ingrese un numero entero: ");
-            Console.WriteLine("hola gustavo ");
+           
             a = int.Parse(Console.ReadLine());
 
             for (int i=1; i<=10; i++)
@@ -17,6 +17,28 @@ namespace TrabajoGit
                 Console.Write( i + " x " + a + "=" + i * a +"\n" );
                 
             }
+
+            Console.WriteLine("hola gustavo ");
+            int A, B, C, D ;
+
+            Console.WriteLine("Ingrese un numero entero: ");
+            Console.WriteLine("hola gustavo ");
+            A = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese un numero entero: ");
+            Console.WriteLine("hola gustavo ");
+            B = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Ingrese un numero entero: ");
+            Console.WriteLine("hola gustavo ");
+            C = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese un numero entero: ");
+            Console.WriteLine("hola gustavo ");
+            D = int.Parse(Console.ReadLine());
+
+            Console.Write((A+B)/(C+D));
+            Console.ReadKey();
         }
     }
 }
