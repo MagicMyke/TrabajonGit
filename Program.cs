4abb57c6071ef04faa54587a619cc7a2b4ff29b2
MagicMyke <mykalexander478@gmail.com>
2021-10-26 10:00:38 -0400

    practiceFinish

--- Program.cs
+++ Program.cs
@@ -7,34 +7,27 @@ namespace TrabajoGit
         static void Main(string[] args)
         {
             int a;
-
             Console.WriteLine("Ingrese un numero entero: ");
-           
             a = int.Parse(Console.ReadLine());
 
             for (int i=1; i<=10; i++)
             {
-                Console.Write( i + " x " + a + "=" + i * a +"\n" );
-                
+                Console.Write( i + " x " + a + "=" + i * a +"\n" );    
             }
 
             Console.WriteLine("hola gustavo ");
             int A, B, C, D ;
 
             Console.WriteLine("Ingrese un numero entero: ");
-            Console.WriteLine("hola gustavo ");
             A = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Ingrese un numero entero: ");
-            Console.WriteLine("hola gustavo ");
             B = int.Parse(Console.ReadLine());
             
             Console.WriteLine("Ingrese un numero entero: ");
-            Console.WriteLine("hola gustavo ");
             C = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Ingrese un numero entero: ");
-            Console.WriteLine("hola gustavo ");
             D = int.Parse(Console.ReadLine());
 
             Console.Write((A+B)/(C+D));
