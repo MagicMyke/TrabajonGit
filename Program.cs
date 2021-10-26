96608f74ce1c5a735f94fb718440ca41f4bc9c5c
MagicMyke <mykalexander478@gmail.com>
2021-10-26 09:17:32 -0400

    trabajoGrupal

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrabajoGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
